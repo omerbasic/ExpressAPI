a777526d74d5e81481517c97090957f65bb1992c
Ensar Omerbasic <omerbasic.ensar@gmail.com>
2020-08-18 18:55:04 +0200

    asd

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Wurld!'))
+//todo resource
+
+app.get('/api/todos', (req,res) => res.json('All todos'))
+app.get('/api/todos/:id', (req,res) => res.json('Specific todos'))
+app.post('/api/todos', (req,res) => res.json('Create todo'))
+app.put('/api/todos/:id', (req,res) => res.json('Update todo'))
+app.delete('/api/todos/:id', (req,res) => res.json('Delete todo'))
+
+app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/about', (req,res) => res.send('Mer om mig'))
 app.listen(3000, () => console.log('Started'))
