d69ccfa6a54910e7593b5e9d6da2a6e98d0396ef
Ensar Omerbasic <omerbasic.ensar@gmail.com>
2020-08-19 14:36:23 +0200

    working list of friends

--- client/script.js
+++ client/script.js
@@ -1,11 +1,38 @@
 window.addEventListener('load', start)
 
 async function start(){
-    const result = await fetch('/api/todos')
-    const todos = await result.json()
-    console.log(todos)
+    const result = await fetch('/api/friends')
+    const friends = await result.json()
+    listFriends(friends)
+    console.log(friends)
+    addEventListeners()
+    
+}
+async function listFriends(friends){
+    const ul = document.querySelector('ul')
+
+    for (const friend of friends){
+        listFriend(friend, ul)
+    }
 }
 
+function addEventListeners(){
+    const button = document.querySelector(button)
+    button.onclick = addFriend
+}
+
+async function addFriend(){
+    console.log("success")
+    event.preventDefault()
+
+    event.target
+}
+
+function listFriend(friend, ul){
+    const li = document.createElement('li')
+    li.innerHTML = JSON.stringify(friend)
+    ul.append(li)
+}
 async function createTodo(event){
     event.preventDefault()
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
 //define database state
-let idIndex = 100
-const todos = [{
+let idIndex = 1
+const friends = [{
     id: 0,
-    title: 'Rasta Hunden',
-    date: '2020-08-18'
+    Firstname: 'Ensar',
+    Lastname: 'Omerbasic'
 }]
 
 const { json } = require('body-parser')
@@ -16,6 +16,7 @@ const app = express()
 //ascertain parsing of body as JSON
 app.use(express.json())
 
+//files taken from /client
 app.use(express.static('client'))
 /* 
 app.get('/', (req, res) => {
@@ -28,26 +29,26 @@ app.get('/client/index.html', (req, res) => res.json('halp')) */
 
 //Todo stuff
 
-app.get('/api/todos', (req, res) => res.json(todos))
-app.get('/api/todos/:id', (req, res) => res.json('Specific todo, id:' + req.params.id))
+app.get('/api/friends', (req, res) => res.json(friends))
+app.get('/api/friends/:id', (req, res) => res.json('Specific friend, id:' + req.params.id))
 
-app.post('/api/todos', (req, res) =>{
-    if (!req.body.title || !req.body.date){
+app.post('/api/friends', (req, res) =>{
+    if (!req.body.Firstname || !req.body.Lastname){
         res.status(400).json({message: 'Data sent as body is not correct'})
     }
     else{
         
-        const todo = {id: idIndex++, ...req.body }
+        const friend = {id: idIndex++, ...req.body }
     
-        todos.push(todo)
-        res.status(201).json(todo)
+        friends.push(friend)
+        res.status(201).json(friend)
         
     }
 
 
 })
-app.put('/api/todos/:id', (req, res) => res.json('Update todo'))
-app.delete('/api/todos/:id', (req, res) => res.json('Delete todo'))
+/* app.put('/api/todos/:id', (req, res) => res.json('Update todo'))
+app.delete('/api/todos/:id', (req, res) => res.json('Delete todo')) */
 
 //middleware 404
 app.use((req, res) => {
