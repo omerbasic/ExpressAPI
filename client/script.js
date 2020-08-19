b946a32577a47717d9dbb46d2bfe5aa9b8db1e52
Ensar Omerbasic <omerbasic.ensar@gmail.com>
2020-08-19 18:28:52 +0200

    cleaned code

--- client/script.js
+++ client/script.js
@@ -1,4 +1,8 @@
+//Starts functionality on page reload
+
 window.addEventListener('load', start)
+
+//initial function, fetches information from server & NASA API
 
 async function start(){
     const result = await fetch('/api/friends')
@@ -7,70 +11,53 @@ async function start(){
     const nasaresult = await fetch('https://api.nasa.gov/planetary/apod?api_key=C36doer5eZSfOmVFy5p4sdaSNys6edZrTfaurYr6')
     const nasa = await nasaresult.json()
 
-  
-
-    console.log(nasa)
-
     addEventListeners()
     listFriends(friends)
 
     const p = document.querySelector('p')
-
     p.innerHTML = JSON.stringify(nasa)
-
-    
-    console.log(friends)
-    
-    
 }
+
+//creates an instance for each friend fetched from server
+
 function listFriends(friends){
     const ul = document.querySelector('ul')
     ul.innerHTML = ''
-    console.log("succ")
     for (const friend of friends){
         listFriend(friend, ul)
     }
 }
 
+// adds functionality to button
+
 function addEventListeners(){
     const button = document.querySelector('button')
     button.onclick = addFriend
-    
-
-
 } 
 
-async function addFriend(){
-    
+//Gets value from input-fields, creates a friend-object with values, calls on functions to POST created friend & reloads page
 
+async function addFriend(){
     const firstName = document.getElementById('1')
     const lastName = document.getElementById('2')
     const friend = {Firstname: firstName.value, Lastname: lastName.value}
-    console.log(friend)
-    console.log("success")
+    
     event.preventDefault()
     event.target
     
     start()
-    
     makeRequest(friend)
 }
+
+//appends each friend found on server to a list
 
 function listFriend(friend, ul){
     const li = document.createElement('li')
     li.innerHTML = JSON.stringify(friend)
     ul.append(li)
 }
-async function createTodo(event){
-    event.preventDefault()
 
-    event.target
-}
-
-
-
-
-    
+//POSTs created friend to server
 
 async function makeRequest(friend) {
     const url = 'http://localhost:3000/api/friends'
@@ -80,7 +67,7 @@ async function makeRequest(friend) {
         method: 'POST',
         body: JSON.stringify(friend)
     })
-    console.log(response)
+    
     const data= await response.json()
     console.log(data)
 }
