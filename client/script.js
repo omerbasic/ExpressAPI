4f7d7dda31a636e23853e6ba92dd84a5d9beb7ef
Ensar Omerbasic <omerbasic.ensar@gmail.com>
2020-08-19 15:38:49 +0200

    my own api is done, external api todo

--- client/script.js
+++ client/script.js
@@ -3,29 +3,47 @@ window.addEventListener('load', start)
 async function start(){
     const result = await fetch('/api/friends')
     const friends = await result.json()
-    listFriends(friends)
-    console.log(friends)
+    
+    
     addEventListeners()
+    listFriends(friends)
+
+    
+    console.log(friends)
+    
     
 }
-async function listFriends(friends){
+function listFriends(friends){
     const ul = document.querySelector('ul')
-
+    ul.innerHTML = ''
+    console.log("succ")
     for (const friend of friends){
         listFriend(friend, ul)
     }
 }
 
 function addEventListeners(){
-    const button = document.querySelector(button)
+    const button = document.querySelector('button')
     button.onclick = addFriend
-}
+    
+
+
+} 
 
 async function addFriend(){
+    
+
+    const firstName = document.getElementById('1')
+    const lastName = document.getElementById('2')
+    const friend = {Firstname: firstName.value, Lastname: lastName.value}
+    console.log(friend)
     console.log("success")
     event.preventDefault()
-
     event.target
+    
+    start()
+    
+    makeRequest(friend)
 }
 
 function listFriend(friend, ul){
@@ -38,3 +56,22 @@ async function createTodo(event){
 
     event.target
 }
+
+
+
+
+    
+
+async function makeRequest(friend) {
+    const url = 'http://localhost:3000/api/friends'
+
+    const response = await fetch(url, {
+        headers: { "Content-Type": "application/json" }, 
+        method: 'POST',
+        body: JSON.stringify(friend)
+    })
+    console.log(response)
+    const data= await response.json()
+    console.log(data)
+}
+
