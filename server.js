bd1b0ae2068f8e0ec83bea2ee28e9e8bb970cf94
Ensar Omerbasic <omerbasic.ensar@gmail.com>
2020-08-19 18:54:33 +0200

    cleaned more code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 //define database state
 let idIndex = 1
+
 const friends = [{
     id: 0,
     Firstname: 'Ensar',
@@ -14,41 +15,26 @@ const app = express()
 
 
 //ascertain parsing of body as JSON
+
 app.use(express.json())
 
 //files taken from /client
+
 app.use(express.static('client'))
-/* 
-app.get('/', (req, res) => {
-    res.send()
-})
 
-
-
-app.get('/client/index.html', (req, res) => res.json('halp')) */
-
-//Todo stuff
+//request handlers
 
 app.get('/api/friends', (req, res) => res.json(friends))
-app.get('/api/friends/:id', (req, res) => res.json('Specific friend, id:' + req.params.id))
-
 app.post('/api/friends', (req, res) =>{
     if (!req.body.Firstname || !req.body.Lastname){
-        res.status(400).json({message: 'Data sent as body is not correct'})
+        res.status(400).json({message: 'Data sent as body is incorrect'})
     }
     else{
-        
         const friend = {id: idIndex++, ...req.body }
-    
         friends.push(friend)
         res.status(201).json(friend)
-        
-    }
-
-
+        }
 })
-/* app.put('/api/todos/:id', (req, res) => res.json('Update todo'))
-app.delete('/api/todos/:id', (req, res) => res.json('Delete todo')) */
 
 //middleware 404
 app.use((req, res) => {
